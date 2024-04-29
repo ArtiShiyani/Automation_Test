8555a97eff47851c84ba450e765954e59a181780
Your Name <you@example.com>
2024-04-30 00:04:47 +0530

    added script for Update and Delete API

--- tests/RestAPITesting-39.spec.js
+++ tests/RestAPITesting-39.spec.js
@@ -1,5 +1,4 @@
 import {test,expect}from '@playwright/test'
-import { waitForDebugger } from 'inspector';
 
 var userid;
 
@@ -11,9 +10,9 @@ test('Get User',async({request})=>{
 
 })
 
-test.only('Create User',async({request})=>{
+test('Create User',async({request})=>{
 
-    const response=await request.post('https://reqres.in//api/users',
+    const response=await request.post('https://reqres.in/api/users',
     {
         data:
         {
@@ -28,17 +27,65 @@ test.only('Create User',async({request})=>{
     })
 
     console.log(await response.json());
-    expect(response.status()).toBe(201);
+    expect(response.status()).toBe(201)
 
     const res = await response.json();
     userid=res.id;
+
+    /*try {
+        const response = await fetch('https://reqres.in//api/users');
+        
+        if (response.status === 404) {
+            // Handle 404 error if needed
+            console.log('Resource not found.');
+            // You might want to throw an error or return early here
+            return;
+        }
+    
+        // Check if the response is in the success range (e.g., 200-299)
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+    
+        const data = await response.json();
+        console.log(data);
+    
+        // Perform your assertion on the response status
+        expect(response.status).toBe(201); // Remove the parentheses after status
+    
+        // Additional handling or assertions with 'data' if needed
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        // Handle or report the error appropriately
+    }*/
+    
 
 })
 
 test('Update User',async({request})=>{
 
+    const response=await request.put('https://reqres.in/api/users'+userid,
+    {
+        data:
+        {
+            "name":"Arti Shiyani",
+            "job":" Senior Manager SDET"
+        },
+        headers:
+        {
+            "Accept":"application/json"
+        }
+
+    })
+
+    console.log(await response.json())
+    expect(response.status()).toBe(201)
+
 })
 
 test('Delete User',async({request})=>{
+
+    const response =await request.delete('https://reqres.in/api/users/'+userid);
+    expect(response.status()).toBe(204);
 
 })
